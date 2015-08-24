d96399a4728b5817c4be909fd61556c6bb61a9b4
Michal Valasek <michal.valasek@gmail.com>
2015-08-24 13:48:28 +0200

    add database support with postgres and sequel

--- Gemfile
+++ Gemfile
@@ -1,5 +1,11 @@
 source "https://rubygems.org"
 
 gem "roda", "~> 2.1.0"
+
+# templating
 gem "tilt", "~> 2.0.1"
 gem "erubis", "~> 2.7.0"
+
+# database
+gem "pg", "~> 0.18.1"
+gem "sequel", "~> 4.21.0"
--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require "roda"
+require "sequel"
 
 class App < Roda
   plugin :static, ["/images", "/css", "/js"]
   plugin :render
   plugin :head
+
+  # DB initialization
+  DB = Sequel.connect(adapter:"postgres", database:"roda_tutorial_dev", host:"127.0.0.1", user:"", password:"")
 
   route do |r|
     r.root do
