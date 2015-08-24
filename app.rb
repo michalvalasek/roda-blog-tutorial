7444ac6819134dded31ed55c99fdc9feb5d4cde9
Michal Valasek <michal.valasek@gmail.com>
2015-08-24 14:18:05 +0200

    secure sessions, add CSRF protection, load models

--- app.rb
+++ app.rb
@@ -1,13 +1,25 @@
 require "roda"
 require "sequel"
+require "bcrypt"
+require "rack/protection"
 
 class App < Roda
   plugin :static, ["/images", "/css", "/js"]
   plugin :render
   plugin :head
 
+  Sequel::Model.plugin :validation_helpers
+
   # DB initialization
   DB = Sequel.connect(adapter:"postgres", database:"roda_tutorial_dev", host:"127.0.0.1", user:"", password:"")
+
+  # security
+  use Rack::Session::Cookie, secret: "someNice_long_r4nd0mString_coi43j2cm(>Cd)42Nc", key: "_roda_tutorial_session"
+  use Rack::Protection
+  plugin :csrf
+
+  # models
+  require './models/user.rb'
 
   route do |r|
     r.root do
