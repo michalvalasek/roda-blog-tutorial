4700493c92f2ff22eb15e729a8b4f209b4d7b0a4
Michal Valasek <michal.valasek@gmail.com>
2015-08-24 13:22:19 +0200

    add layout, views and routes for the basic pages (home, about, contact)

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 require "roda"
 
 class App < Roda
+  plugin :static, ["/images", "/css", "/js"]
+  plugin :render
+  plugin :head
 
   route do |r|
     r.root do
-      "Hello!"
+      view "homepage"
+    end
+
+    r.get "about" do
+      view "about"
+    end
+
+    r.get "contact" do
+      view "contact"
     end
   end
 end
