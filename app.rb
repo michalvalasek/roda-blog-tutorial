d9d74182b678d8bd01bdd10b0844ea3c79c5b51f
Michal Valasek <michal.valasek@gmail.com>
2015-08-25 18:47:30 +0200

    Add user routes.

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Roda
   Sequel::Model.plugin :validation_helpers
 
   # DB initialization
-  DB = Sequel.connect(adapter:"postgres", database:"roda_tutorial_dev", host:"127.0.0.1", user:"", password:"")
+  DB = Sequel.connect(adapter:"postgres", database:"roda_tutorial_dev", host:"127.0.0.1", user:"michal", password:"")
 
   # security
   use Rack::Session::Cookie, secret: "someNice_long_r4nd0mString_coi43j2cm(>Cd)42Nc", key: "_roda_tutorial_session"
@@ -32,6 +32,52 @@ class App < Roda
 
     r.get "contact" do
       view "contact"
+    end
+
+    r.get "login" do
+      view "login"
+    end
+
+    r.post "login" do
+      if user = User.authenticate(r["email"],r["password"])
+        session[:user_id] = user.id
+        r.redirect "/"
+      else
+        r.redirect "/login"
+      end
+    end
+
+    r.post "logout" do
+      session.clear
+      r.redirect "/"
+    end
+
+    r.on "users" do
+      r.get "new" do
+        @user = User.new
+        view "users/new"
+      end
+
+      r.get ":id" do |id|
+        @user = User[id]
+        view "users/show"
+      end
+
+      r.is do
+        r.get do
+          @users = User.order(:id)
+          view "users/index"
+        end
+
+        r.post do
+          @user = User.new(r["user"])
+          if @user.valid? && user.save
+            r.redirect "/users"
+          else
+            view "users/new"
+          end
+        end
+      end
     end
   end
 end
