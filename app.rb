0c97eb1bc48bd44672857533fe562e9798cd7055
Michal Valasek <michal.valasek@gmail.com>
2015-08-25 19:22:53 +0200

    Add Posts management.

--- app.rb
+++ app.rb
@@ -8,7 +8,9 @@ class App < Roda
   plugin :render
   plugin :head
 
+  # load Sequel plugins
   Sequel::Model.plugin :validation_helpers
+  Sequel::Model.plugin :timestamps, update_on_create: true
 
   # DB initialization
   DB = Sequel.connect(adapter:"postgres", database:"roda_tutorial_dev", host:"127.0.0.1", user:"michal", password:"")
@@ -20,11 +22,15 @@ class App < Roda
 
   # models
   require './models/user.rb'
+  require './models/post.rb'
 
   route do |r|
     r.root do
+      @posts = Post.reverse_order(:created_at)
       view "homepage"
     end
+
+    # static pages
 
     r.get "about" do
       view "about"
@@ -33,6 +39,16 @@ class App < Roda
     r.get "contact" do
       view "contact"
     end
+
+    # blog
+
+    r.get /posts\/([0-9]+)/ do |id|
+      @post = Post[id]
+      @user_name = @post.user.name
+      view "posts/show"
+    end
+
+    # login / logout
 
     r.get "login" do
       view "login"
@@ -50,6 +66,12 @@ class App < Roda
     r.post "logout" do
       session.clear
       r.redirect "/"
+    end
+
+    # ADMIN AREA after this line
+
+    unless session[:user_id]
+      r.redirect "/login"
     end
 
     r.on "users" do
@@ -71,10 +93,44 @@ class App < Roda
 
         r.post do
           @user = User.new(r["user"])
-          if @user.valid? && user.save
+          if @user.valid? && @user.save
             r.redirect "/users"
           else
             view "users/new"
+          end
+        end
+      end
+    end
+
+    r.on "posts" do
+      r.get "new" do
+        @post = Post.new
+        view "posts/new"
+      end
+
+      r.post do
+        @post = Post.new(r["post"])
+        @post.user = User[session[:user_id]]
+
+        if @post.valid? && @post.save
+          r.redirect "/"
+        else
+          view "posts/new"
+        end
+      end
+
+      r.on ":id" do |id|
+        @post = Post[id]
+
+        r.get "edit" do
+          view "posts/edit"
+        end
+
+        r.post do
+          if @post.update(r["post"])
+            r.redirect "/posts/#{@post.id}"
+          else
+            view "posts/edit"
           end
         end
       end
